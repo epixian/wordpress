9dec7ef6a866e4699941c0180a02c2df4a54ba73
Erich Oelschlegel (EPIXIAN LLC) <epixianllc@gmail.com>
2018-06-21 15:12:46 +0000

    Cleaned up comments

--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
+# resolves console output warnings during initial server setup
 echo "console-setup   console-setup/charmap47 select  UTF-8" > encoding.conf
 debconf-set-selections encoding.conf
 rm encoding.conf
 export TERM=xterm
 export DEBIAN_FRONTEND=noninteractive
+
+# update apt repositories
 echo "Updating package lists..."
 apt-get -qq update
 echo "Upgrading existing installed packages..."
 apt-get -qq upgrade -y
+
+# install LAMP essentials
 echo "Installing required programs/libraries..."
 apt-get -qq install awscli apache2 mysql-client php libapache2-mod-php php-mcrypt php-mysql php-curl php-mbstring php-gd php-xml php-xmlrpc php-zip -y
-# pip install awscli --upgrade
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php
 systemctl enable apache2
 echo "*** init.sh execution complete ***" | wall
